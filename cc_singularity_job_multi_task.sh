6a30907114d28f31d49261099319bdb2b70f8341
Andreas M. Munk <andreas.m.m64@gmail.com>
2019-09-02 15:19:55 -0700

    redo stuff

--- cc_singularity_job_multi_task.sh
+++ cc_singularity_job_multi_task.sh
@@ -90,7 +90,7 @@ mem_per_task=$((${SLURM_MEM_PER_NODE} / ${SLURM_NTASKS}))
 if [[ $GPUS_PER_TASK -ge 1 ]]; then
     srun_options="-n1 -N1 --gres=gpu:${GPUS_PER_TASK} --exclusive --mem=${mem_per_task}"
 else
-    srun_options="-n1 -N1 --mem=${mem_per_task}"
+    srun_options="-n1 -N1 --exclusive --mem=${mem_per_task}"
 fi
 
 IFS=' ' read -a srun_options <<< "$srun_options"
