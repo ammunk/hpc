1e8e230b680fa53e59294e1e5840167f671c26c4
Andreas M. Munk <andreas.m.m64@gmail.com>
2019-09-02 23:36:04 -0700

    more debugging

--- cc_singularity_job_multi_task.sh
+++ cc_singularity_job_multi_task.sh
@@ -90,12 +90,13 @@ mem_per_task=$((${SLURM_MEM_PER_NODE} / ${SLURM_NTASKS}))
 if [[ $GPUS_PER_TASK -ge 1 ]]; then
     srun_options="-n1 -N1 --gres=gpu:${GPUS_PER_TASK} --exclusive --mem=${mem_per_task} --cpu-bind=cores"
 else
-    srun_options="-n1 -N1 --exclusive --mem=${mem_per_task} --cpu-bind=cores --network=Instances=2"
+    srun_options="-n1 -N1 --exclusive --mem=${mem_per_task} --cpu-bind=cores --network=Instances=6"
 fi
 
 IFS=' ' read -a srun_options <<< "$srun_options"
 
 counter=1
+echo $srun_options
 for cmd in "${CMDs[@]}"; do
     # --nv option: bind to system libraries (access to GPUS etc.)
     # --no-home and --containall mimics the docker container behavior
