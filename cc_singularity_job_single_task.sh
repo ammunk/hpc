0542b2f869da7dcbe2e4770164b1c51ea2e47463
Andreas M. Munk <andreas.m.m64@gmail.com>
2019-09-03 01:11:06 -0700

    updates

--- cc_singularity_job_single_task.sh
+++ cc_singularity_job_single_task.sh
@@ -102,8 +102,8 @@ fi
 results_to_tar_suffix=$(tr ' |/' '_' <<< ${RESULTS_TO_TAR[@]})
 
 # make a tarball of the results
-time tar -cf "tar_ball_${results_to_tar_suffix_${SLURM_JOB_ID}}.tar" ${RESULTS_TO_TAR[@]}
+time tar -cf "tar_ball_${results_to_tar_suffix}_${SLURM_JOB_ID}.tar" ${RESULTS_TO_TAR[@]}
 
 # move unpack the tarball to the BASERESULTSDIR
 cd $BASERESULTSDIR
-tar --keep-newer-files -xf "${SLURM_TMPDIR}/tar_ball_${results_to_tar_suffix_${SLURM_JOB_ID}}.tar"
+tar --keep-newer-files -xf "${SLURM_TMPDIR}/tar_ball_${results_to_tar_suffix}_${SLURM_JOB_ID}.tar"
