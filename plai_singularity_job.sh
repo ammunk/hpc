cd9fb162376ef3ed6931b0d33c22b82256e150be
Andreas M. Munk <andreas.m.m64@gmail.com>
2020-05-06 10:19:43 -0700

    update hpc scripts

--- plai_singularity_job.sh
+++ plai_singularity_job.sh
@@ -104,22 +104,22 @@ SINGULARITYENV_SLURM_JOB_ID=$SLURM_JOB_ID \
 
 ######################################################################
 
-if [ -z ${RESULTS_TO_TAR} ]; then
+if [ -z "${RESULTS_TO_TAR}" ]; then
     # IF NO RESULTS TO TAR IS SPECIFIED - MAKE A TARBALL OF THE ENTIRE RESULTS DIRECTORY
     RESULTS_TO_TAR=("results")
 else
     # if variable is provided make into an array
-    IFS=' ' read -a RESULTS_TO_TAR <<< $RESULTS_TO_TAR
+    IFS=' ' read -a RESULTS_TO_TAR <<< "${RESULTS_TO_TAR[@]}"
 fi
 
 # replace any "/"-character or spaces with "_" to use as a name
-results_to_tar_suffix=$(tr ' |/' '_' <<< ${RESULTS_TO_TAR[@]})
+results_to_tar_suffix="$(tr ' |/' '_' <<< ${RESULTS_TO_TAR[@]})"
 
 # make a tarball of the results
-time tar -cf "tar_ball_${results_to_tar_suffix}_${SLURM_JOB_ID}.tar" ${RESULTS_TO_TAR[@]}
+time tar -cf "tar_ball_${results_to_tar_suffix}_${SLURM_JOB_ID}.tar" "${RESULTS_TO_TAR[@]}"
 
 # move unpack the tarball to the BASERESULTSDIR
-cd $BASERESULTSDIR
+cd "$BASERESULTSDIR"
 time tar --keep-newer-files -xf "${PLAI_TMPDIR}/tar_ball_${results_to_tar_suffix}_${SLURM_JOB_ID}.tar"
 
 ######################################################################
