8f588b8d2d91db06e4c706a4a2762ecde156f38d
Andreas Munk <amunk@borg.cs.ubc.ca>
2020-09-16 21:22:22 -0700

    update cleanup files

--- plai_cleanup.sh
+++ plai_cleanup.sh
@@ -2,5 +2,11 @@
 
 PLAI_TMPDIR="/scratch-ssd/"
 cd $PLAI_TMPDIR
-echo "CHECK WHATS IN /scratch-ssd:" && ls
-# rm -rf amunk*
+echo "WHATS IN /scratch-ssd BEFORE CLEANUP:" && ls -la
+# see https://stackoverflow.com/questions/2937407/test-whether-a-glob-has-any-matches-in-bash
+if test -n "$(find -maxdepth 1 -name "amunk*" -print -quit)"; then
+    rm -rf amunk*
+    echo "WHATS IN /scratch-ssd AFTER CLEANUP:" && ls -la
+else
+    echo "Nothing to delete"
+fi
